dbc83fff02c9f135021397fbc7fb255e7b4ed546
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-07-24 22:11:11 +0200

    checked if Application works well with empty HomeController object.

---
+++ src/main/java/com/codingseahorse/tastylab/pages/home/HomeController.java
@@ -0,0 +1,9 @@
+package com.codingseahorse.tastylab.pages.home;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/home")
+public class HomeController {
+}
