a9ffa54c588ecbe3b2871b6692a49a9a5694fd68
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-07-24 21:29:50 +0200

    starting point

---
+++ src/main/java/com/codingseahorse/tastylab/TastylabApplication.java
@@ -0,0 +1,13 @@
+package com.codingseahorse.tastylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastylabApplication.class, args);
+	}
+
+}
