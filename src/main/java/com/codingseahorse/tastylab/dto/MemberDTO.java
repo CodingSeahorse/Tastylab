09dd4bd5a5599c87de613aa676b1cf00aac3f4b7
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-09-29 16:27:19 +0200

    clean up

--- src/main/java/com/codingseahorse/tastylab/dto/MemberDTO.java
+++ src/main/java/com/codingseahorse/tastylab/dto/MemberDTO.java
@@ -2,20 +2,17 @@ package com.codingseahorse.tastylab.dto;
 
 import com.codingseahorse.tastylab.model.member.Gender;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 
 @Data
-@NoArgsConstructor
 @RequiredArgsConstructor
 public class MemberDTO {
-    private @NonNull String firstName;
-    private @NonNull String lastName;
+    private final String firstName;
+    private final String lastName;
     private String email;
-    private @NonNull Integer age;
-    private @NonNull Gender gender;
+    private final Integer age;
+    private final Gender gender;
     private Page<RecipeDTO> recipes;
     private MemberCardDTO memberCardDTO;
 }
