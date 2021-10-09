8b6f1134d65edec88c2ac63000e46b6e8d1c4ec7
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-10-09 16:47:31 +0200

    edited sidebar.jsx and sidebar.scss

--- src/frontend/src/components/nav-elements/sidebar/sidebar.jsx
+++ src/frontend/src/components/nav-elements/sidebar/sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import './sidebar.scss';
-import {Link, useHistory} from "react-router-dom";
+import {
+    Link,
+    useHistory} from "react-router-dom";
 
+import './sidebar.scss';
+import 'tippy.js/dist/tippy.css';
+
+import Tippy from "@tippyjs/react";
 import AuthenticationService from "../../../services/AuthenticationService";
 
 const Sidebar = ({isLoggedIn}) => {
@@ -10,43 +15,73 @@ const Sidebar = ({isLoggedIn}) => {
         return (
             <nav className="container">
                 <div className="container__elements">
-                    <Link to="/welcome/login">
-                        <div className="container__elements__member"/>
+                    <Link to="/welcome/login"> {/*TODO:REPLACE WITH PROFILE PAGE*/}
+                        <Tippy themeinteractive={true} content="Profile" style={{color:'green'}}>
+                            <div className="container__elements__member" id="test"/>
+                        </Tippy>
                     </Link>
                     <Link to="/home">
-                        <div className="container__elements__home"/>
+                        <Tippy themeinteractive={true} content="Home">
+                            <div className="container__elements__home"/>
+                        </Tippy>
                     </Link>
-                    <div className="container__elements__search"/>
-                    <div className="container__elements__wishlist"/>
-                    <div className="container__elements__lizzy"/>
-                    <div className="container__elements__write"/>
-                    <div className="container__elements__option"/>
-                    <div className="container__elements__logout"
-                         onClick={() => {
-                                 AuthenticationService.logout()
-                                 setTimeout(
-                                     () => {history.push("/welcome/login")},
-                                     2000)}}/>
+                    <Tippy themeinteractive={true} content="Search">
+                        <div className="container__elements__search"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Wishlist">
+                        <div className="container__elements__wishlist"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Lizzy">
+                        <div className="container__elements__lizzy"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Write">
+                        <div className="container__elements__write"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Option">
+                        <div className="container__elements__option"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Logout">
+                        <div className="container__elements__logout"
+                             onClick={() => {
+                                     AuthenticationService.logout()
+                                     setTimeout(
+                                         () => {history.push("/welcome/login")},
+                                         2000)}}/>
+                    </Tippy>
                 </div>
             </nav>
         )
     }else {
         return (
-            <nav className="container">
+            <nav className="container" id="no-user">
                 <div className="container__elements">
                     <Link to="/welcome/login">
-                        <div className="container__elements__member">
-                            <p>Login</p>
-                        </div>
+                        <Tippy themeinteractive={true} content="Login">
+                            <div className="container__elements__member" id="no-user-login">
+                                <p>Login</p>
+                            </div>
+                        </Tippy>
                     </Link>
                     <Link to="/home">
-                        <div className="container__elements__home"/>
+                        <Tippy themeinteractive={true} content="Home">
+                            <div className="container__elements__home"/>
+                        </Tippy>
                     </Link>
-                    <div className="container__elements__search"/>
-                    <div className="container__elements__wishlist"/>
-                    <div className="container__elements__lizzy"/>
-                    <div className="container__elements__write"/>
-                    <div className="container__elements__option"/>
+                    <Tippy themeinteractive={true} content="Search">
+                        <div className="container__elements__search"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Wishlist">
+                        <div className="container__elements__wishlist"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Lizzy">
+                        <div className="container__elements__lizzy"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Write">
+                        <div className="container__elements__write"/>
+                    </Tippy>
+                    <Tippy themeinteractive={true} content="Option">
+                        <div className="container__elements__option"/>
+                    </Tippy>
                 </div>
             </nav>
         )
